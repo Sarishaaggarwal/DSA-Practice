cdbed37b2fa10bbe604ed1fdf636bd706854e343
Sarishaaggarwal <76108857+Sarishaaggarwal@users.noreply.github.com>
2022-08-05 13:15:33 +0530

    Create printLongestCommonSubsequence.cpp

---
+++ Dynamic Programming/printLongestCommonSubsequence.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t[101][101];
+string print_lcs(string x,string y,int n,int m){
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<m+1;j++){
+            if(i==0||j==0){
+                t[i][j]=0;
+            }
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(x[i-1]==y[j-1]){
+                t[i][j]=1+t[i-1][j-1];
+            }
+            else{
+                t[i][j]=max(t[i-1][j],t[i][j-1]);
+            }
+        }
+    }
+    string s;
+    int i=n;
+    int j=m;
+    while(i>0 && j>0){
+        if(x[i-1]==y[j-1]){
+            s+=x[i-1];
+            i--;
+            j--;
+        }
+        else{
+            if(t[i][j-1]>t[i-1][j]){
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+    reverse(s.begin(),s.end());
+    return s;
+    
+}
+
+int main(){
+    string x="sarisha";
+    string y="saris";
+    int n=x.length();
+    int m=y.length();
+    cout<<print_lcs(x,y,n,m)<<endl;
+    return 0;
+}
