666cf0897a3f03516d16678ef456a96961319adb
Sarishaaggarwal <76108857+Sarishaaggarwal@users.noreply.github.com>
2022-08-07 10:04:15 +0530

    Create palindromePartitioning.cpp

---
+++ Dynamic Programming/palindromePartitioning.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalindrome(string s,int i,int j){
+    while(i<j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+int palindromePartitioning(string s,int i,int j){
+    if(i>=j){
+        return 0;
+    }
+    if(isPalindrome(s,i,j)==true){
+        return 0;
+    }
+    int m=INT_MAX;
+    for(int k=i;k<=j-1;k++){
+        int temp= palindromePartitioning(s,i,k) + palindromePartitioning(s,k+1,j) + 1;
+        if(temp<m){
+            m=temp;
+        }
+    }
+    return m;
+}
+int main(){
+    string s="abcbe";
+    int i=0;
+    int j=s.length()-1;
+    cout<<palindromePartitioning(s,i,j)<<endl;
+    return 0;
+}
